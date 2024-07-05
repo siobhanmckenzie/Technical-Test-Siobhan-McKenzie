12485b798b2318818164ac5f477a111640aada37
Siobhan <38178307+siobhanmckenzie@users.noreply.github.com>
2024-07-05 10:01:53 +0100

    chore: remove tailwind from index page

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -6,7 +6,7 @@ export const Route = createLazyFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
+    <div>
       <h3>9fin Technical Test - Siobhan McKenzie</h3>
     </div>
   );
