7fcb390e24546bc15572ef6de13cf225dbd2251b
Siobhan <38178307+siobhanmckenzie@users.noreply.github.com>
2024-07-05 10:01:29 +0100

    chore: cleanup - remove DesignSystem and tailwind css on index

--- src/shared/DesignSystem/DesignSystem.styles.ts
+++
@@ -1,8 +0,0 @@
-// this represents shared container and other elements that might come from a design system (possibly a private npm package)
-// may refactor later on
-
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  padding: 20px;
-`;
